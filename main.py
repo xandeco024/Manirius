1a5fa56adc1b8242512ea735a55d06baeadd09fe
Xandeco Odyssey <alepbravo1702@gmail.com>
2023-09-12 22:11:36 -0300

    projeto grid

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ tileset = pygame.image.load(tilesetSprite).convert_alpha()
 map = [
     [31,22,63,22,22,22,22,22,22,33],
     [13,42,71,42,42,42,42,42,42,11],
-    [13,42,71,42,82,42,42,42,42,11],
-    [13,42,81,42,42,42,42,42,42,11],
-    [13,42,42,42,42,42,42,42,42,11],
+    [13,42,71,42,71,42,42,42,42,11],
+    [13,42,85,95,92,42,42,42,42,11],
+    [13,42,42,42,71,42,42,42,42,11],
     [13,42,42,42,42,83,42,42,42,11],
     [13,42,42,42,42,42,42,42,42,11],
     [13,42,42,42,42,42,91,92,92,72],
@@ -88,6 +88,44 @@ def animate(sprites, frameDuration):
 
     return sprite
 
+def collisionTest(rect , tiles):
+    hitList = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hitList.append(tile)
+    return hitList
+
+def move(rect, movement, tiles):
+    collisionTypes = {'top': False, 'bottom': False, 'left': False, 'right': False}
+
+    rect.x += movement[0]
+
+    hitList = collisionTest(rect, tiles)
+    for tile in hitList:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collisionTypes['right'] = True
+
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collisionTypes['left'] = True
+
+    rect.y += movement[1]
+    hitList = collisionTest(rect, tiles)
+    for tile in hitList:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collisionTypes['bottom'] = True
+
+        if movement[1] < 0:
+            rect.top = tile.bottom
+            collisionTypes['top'] = True
+    return rect, collisionTypes
+
+def inputList():
+    inputList = pygame.key.get_pressed()
+    return inputList
+
 playerStartPos = (3/2 * 64, 3/2 * 64)
 
 font = pygame.font.SysFont('Arial', 20)
@@ -104,6 +142,41 @@ tilesetSize = tileset.get_size()
 tiles.insert(0, pygame.Surface((tileWidth,tileHeight)).convert_alpha())
 spritesCortados = cutSpritesheet(tilesetSprite, 64, 64)
 tiles.extend(spritesCortados)
+#endregion
+
+playerVelocity = [0,0]
+
+#region selectTile
+
+selectedTile = pygame.Surface((tileWidth, tileHeight))
+selectedTile.fill((255, 255, 255))
+selectedTile.set_alpha(100)
+
+def calcMousePos(map):
+    mapXSize = len(map[0])
+    mapYSize = len(map)
+
+    mousePosition = pygame.mouse.get_pos()
+    mouseTilePos = (0,0)
+
+
+    for x in range(mapXSize):
+        if mousePosition[0] >= x * 64 and mousePosition[0] <= (x + 1) * 64:
+            mouseTilePos = (x, mouseTilePos[1])
+            break
+        
+    for y in range(mapYSize):
+        if mousePosition[1] >= y * 64 and mousePosition[1] <= (y + 1) * 64:
+            mouseTilePos = (mouseTilePos[0], y)
+            break
+
+    if map[mouseTilePos[1]] [mouseTilePos[0]] == 42:
+        selectedTile.fill((255, 255, 255))
+
+    else:
+        selectedTile.fill((255, 0, 0))
+
+    return (mouseTilePos[0] * 64, mouseTilePos[1] * 64)
 #endregion
 
 while True: #Game Loop
@@ -139,7 +212,6 @@ while True: #Game Loop
 
     #desenha o mapa
     
-
     tileRects = []
     y = 0
     for row in map:
@@ -155,6 +227,22 @@ while True: #Game Loop
     #tilemap = pygame.transform.scale(tilemap, (640,640))
     screen.blit(tilemap, (0,0))
 
+    if(inputList()[pygame.K_w]):
+        playerVelocity[1] = -player.playerSpeed
+    elif(inputList()[pygame.K_s]):
+        playerVelocity[1] = player.playerSpeed
+    else:
+        playerVelocity[1] = 0
+
+    if(inputList()[pygame.K_a]):
+        playerVelocity[0] = -player.playerSpeed
+    elif(inputList()[pygame.K_d]):
+        playerVelocity[0] = player.playerSpeed
+    else:
+        playerVelocity[0] = 0
+
+    playerRect, collisions = move(player.rect, playerVelocity, tileRects)
+
 
 
     #region Score
@@ -168,9 +256,8 @@ while True: #Game Loop
 
     screen.blit(scoreTextSurface, scoreTextRect)
     attractiveObjects.draw(screen)
-
+    screen.blit(selectedTile, calcMousePos(map))
     playerRotatedImg = pygame.transform.rotate(player.image, angle)
-    playerRotatedImgRect = playerRotatedImg.get_rect(center = player.rect.center)
-    screen.blit(playerRotatedImg, playerRotatedImgRect)
+    screen.blit(playerRotatedImg, (playerRect.x, playerRect.y))
 
     pygame.display.update() #Atualiza a tela
